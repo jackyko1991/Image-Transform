1862c10e7e5c2257ffc95bf384a9992a1a6782ca
Ka Long Ko <jackkykokoko@gmail.com>
2019-08-07 15:04:43 +0800

    2d complete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import SimpleITK as sitk
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
+import math
 
 def load_img(path,dimension,view=False):
 	# load image data
@@ -39,13 +41,10 @@ def load2D():
 def load3D():
 	return None
 
-def transform2D(image):
+def transform2D(image, affine_matrix):
 	# grab the shape of the image
 	B, H, W, C = image.shape
-	# initialize M to identity transform
-	M = np.array([[1., 0., 0.], [0., 1., 0.]])
-	# repeat num_batch times
-	M = np.resize(M, (B, 2, 3))
+	M = affine_matrix
 
 	# mesh grid generation
 	x = np.linspace(-1, 1, W)
@@ -59,12 +58,56 @@ def transform2D(image):
 	# repeat to number of batches
 	sampling_grid = np.resize(sampling_grid, (B, 3, H*W))
 
-	print(sampling_grid.shape)
-
 	# transform the sampling grid, i.e. batch multiply
-	batch_grids = np.matmul(M, sampling_grid)
-	exit()
+	batch_grids = np.matmul(M, sampling_grid) # the batch grid has the shape (B, 2, H*W)
 
+	# reshape to (B, H, W, 2)
+	batch_grids = batch_grids.reshape(B, 2, H, W)
+	batch_grids = np.moveaxis(batch_grids,1,-1)
+
+	# bilinear resampler
+	x_s = batch_grids[:,:,:,0:1].squeeze()
+	y_s = batch_grids[:,:,:,1:2].squeeze()\
+
+	# rescale x and y to [0, W/H]
+	x = ((x_s+1.)*W)*0.5
+	y = ((y_s+1.)*H)*0.5
+
+	# for each coordinate we need to grab the corner coordinates
+	x0 = np.floor(x).astype(np.int64)
+	x1 = x0+1
+	y0 = np.floor(y).astype(np.int64)
+	y1 = y0+1
+
+	# clip to fit actual image size
+	x0 = np.clip(x0, 0, W-1)
+	x1 = np.clip(x1, 0, W-1)
+	y0 = np.clip(y0, 0, H-1)
+	y1 = np.clip(y1, 0, H-1)
+
+	# grab the pixel value for each corner coordinate
+	Ia = image[np.arange(B)[:,None,None], y0, x0]
+	Ib = image[np.arange(B)[:,None,None], y1, x0]
+	Ic = image[np.arange(B)[:,None,None], y0, x1]
+	Id = image[np.arange(B)[:,None,None], y1, x1]
+
+	# calculated the weighted coefficients and actual pixel value
+	wa = (x1-x) * (y1-y)
+	wb = (x1-x) * (y-y0)
+	wc = (x-x0) * (y1-y)
+	wd = (x-x0) * (y-y0)
+
+	# add dimension for addition
+	wa = np.expand_dims(wa, axis=3)
+	wb = np.expand_dims(wb, axis=3)
+	wc = np.expand_dims(wc, axis=3)
+	wd = np.expand_dims(wd, axis=3)
+
+	# compute output
+	image_out = wa*Ia + wb*Ib + wc*Ic + wd*Id
+	image_out = image_out.astype(np.int64)
+
+	return image_out
 
 def transform3D(image):
 	# grab the shape of the image
@@ -79,12 +122,53 @@ def main():
 	if MODE == '2D':
 		# 2D
 		input_img = load2D()
-		transform2D(input_img)
+
+		# define the affine matrix
+		# initialize M to identity transform
+		M = np.array([[1., 0., 0.], [0., 1., 0.]])
+		# repeat num_batch times
+		M = np.resize(M, (input_img.shape[0], 2, 3))
+
+		# change affine matrix values
+		# translation
+		M[0,:,:] = [[1,0,0.5],[0,1,0.25]]
+		img_translate = transform2D(input_img, M)
+
+		# rotation
+		angle = 45 #degree
+		M[0,:,:] = [[math.cos(angle/180*math.pi),-math.sin(angle/180*math.pi),0],[math.sin(angle/180*math.pi),math.cos(angle/180*math.pi),0]]
+		img_rotate = transform2D(input_img, M)
+
+		# shear
+		M[0,:,:] = [[1,0.5,0],[0.5,1,0]]
+		img_shear = transform2D(input_img, M)
+
+		plt.figure(1)
+		ax1 = plt.subplot(221)
+		plt.imshow(input_img[0,:,:,:], cmap="gray")
+		ax1.title.set_text('Original')
+		plt.axis("off")
+
+		ax2 =plt.subplot(222)
+		plt.imshow(img_translate[0,:,:,:], cmap="gray")
+		ax2.title.set_text('Translation')
+		plt.axis("off")
+
+		ax3 = plt.subplot(223)
+		plt.imshow(img_rotate[0,:,:,:], cmap="gray")
+		ax3.title.set_text('Rotation')
+		plt.axis("off")
+
+		ax4 = plt.subplot(224)
+		plt.imshow(img_shear[0,:,:,:], cmap="gray")
+		ax4.title.set_text('Shear')
+		plt.axis("off")
+
+		plt.show()
 	else:
 		# 3D
 		input_img = load3D() 
 		transform3D(input_img)
-
 
 
 if __name__=="__main__":
